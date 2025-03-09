9c315c002d76f328937d2bdccadcb756ae884b82
Matt Moayeri <notron021@gmail.com>
2025-03-09 13:24:59 +0100

    initial commit

---
+++ NeugelbTests/NeugelbTests.swift
@@ -0,0 +1,17 @@
+//
+//  NeugelbTests.swift
+//  NeugelbTests
+//
+//  Created by Matt on 09.03.25.
+//
+
+import Testing
+@testable import Neugelb
+
+struct NeugelbTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
